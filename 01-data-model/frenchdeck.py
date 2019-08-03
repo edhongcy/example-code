fd3c60574fd4dcb589e2b0bcc8fbe78421317aec
ED Hong <edhong@qnap.com>
2019-08-03 14:43:06 +0800

    doc: review ch01

--- 01-data-model/frenchdeck.py
+++ 01-data-model/frenchdeck.py
@@ -1,17 +1,20 @@
 import collections
 
+# namedtuple define Card
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'spades diamonds clubs hearts'.split()
 
+    # 起始化牌堆
     def __init__(self):
         self._cards = [Card(rank, suit) for suit in self.suits
                                         for rank in self.ranks]
-
+    # 用 len 計算 list 個數
     def __len__(self):
         return len(self._cards)
-
+    # 從 postition 去找牌花色及數字
     def __getitem__(self, position):
         return self._cards[position]
+
--- 01-data-model/vector2d.py
+++ 01-data-model/vector2d.py
@@ -6,15 +6,18 @@ class Vector:
         self.x = x
         self.y = y
 
+    # 顯示數字（沒有透過 repr 顯示返回的値是 type）
     def __repr__(self):
         return 'Vector(%r, %r)' % (self.x, self.y)
 
+    # 算斜邊長
     def __abs__(self):
         return hypot(self.x, self.y)
 
     def __bool__(self):
         return bool(abs(self))
 
+    # 兩個向量相加
     def __add__(self, other):
         x = self.x + other.x
         y = self.y + other.y
@@ -22,3 +25,7 @@ class Vector:
 
     def __mul__(self, scalar):
         return Vector(self.x * scalar, self.y * scalar)
+
+# repr & srt 差別
+# __repr__ goal is to be unambiguous
+# __str__ goal is to be readable
